cd588281318ae0a94e97d65436d49b2905934d6e
N M Babu <nmbabu6040@gmail.com>
2026-09-24 17:00:56 +0600

    Force HTTPS scheme in production to fix mixed content on assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\Category;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,6 +30,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
 
         View::composer('layouts.site', function ($view) {
             $view->with(
